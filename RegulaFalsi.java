61c7cec1ec5466350ee1bc87d347a3aaffed28dd
Anushrey-Badoni <badoni.anushrey@gmail.com>
2026-05-03 20:31:46 +0530

    Update RegulaFalsi.java

--- RegulaFalsi.java
+++ RegulaFalsi.java
@@ -1,45 +1,21 @@
-class Main {
-    public static double equate(double x) {
-        return x * Math.log10(x) - 1.2;
-    }
-    
-    public static double approx(double x0, double x1) {
-        return x0 - ((x1 - x0) / (equate(x1) - equate(x0))) * equate(x0);
-    }
-    
+public class RegulaFalsi {
+    static double f(double x) { return x * Math.log10(x) - 1.2; }
+
     public static void main(String[] args) {
-        int i = 1;
-        double positive = 0;
-        double negative = 0;
-        
-        while (true) {
-            if (equate(i) < 0) {
-                negative = i;
-            }
-            else {
-                positive = i;
-                break;
-            }
-            i++;
+        double a = 1, b = 1;
+        while (f(b) < 0) {
+            a = b;
+            b++;
         }
-        
-        double curr = 0;
-        
-        for (i = 0; i < 4; i++) {
-            curr = approx(negative, positive);
-            double root = equate(curr);
+
+        double c = 0;
+        for (int i = 0; i < 10; i++) {
+            c = (a * f(b) - b * f(a)) / (f(b) - f(a));
             
-            if (root < 0) {
-                negative = curr;
-            }
-            else if (root == 0) {
-                break;
-            }
-            else {
-                positive = curr;
-            }
+            if (f(c) < 0) a = c;
+            else b = c;       
         }
-        
-        System.out.println(curr);
+
+        System.out.println(c);
     }
 }
